10f4c485d299f4469db391d5efe631e971a12033
Ztaal <martin.staal.steenberg@gmail.com>
2014-10-08 21:42:27 +0200

    Changed setID
    Changed setID and cleaned up code a bit

--- DataLinkLayer.cpp
+++ DataLinkLayer.cpp
@@ -31,14 +31,14 @@ void getDatagramsWraper(DataLinkLayer *DaLLObj)
 void DataLinkLayer::bitStuff(vector<bool> &frame)
 {
     vector<int> elementsToStuff;
-    for(int i=flag.size()-1;i<frame.size();i++)
+    for(int i = flag.size() - 1; i < frame.size(); i++)
     {
-        if(flagcheck(frame,i-((int)flag.size()-1),flag,flag.size()-1)) elementsToStuff.push_back(i);
+        if(flagcheck(frame, i - ((int)flag.size() - 1), flag,flag.size() - 1)) elementsToStuff.push_back(i);
     }
-    int offset=0;
-    for(int index : elementsToStuff) 
+    int offset = 0;
+    for(int index : elementsToStuff)
     {
-        frame.insert(frame.begin()+offset+index,!flag.back());
+        frame.insert(frame.begin() + offset + index, !flag.back());
         offset++;
     }
 }
@@ -47,14 +47,14 @@ void DataLinkLayer::bitStuff(vector<bool> &frame)
 void DataLinkLayer::revBitStuff(vector<bool> &frame)
 {
     vector<int> elementsToRemove;
-    for(int i=flag.size()-1;i<frame.size();i++)
+    for(int i = flag.size() - 1; i < frame.size(); i++)
     {
-        if (flagcheck(frame,i-(int)flag.size()+1,flag,flag.size()-1)) elementsToRemove.push_back(i);
+        if (flagcheck(frame, i - (int)flag.size() + 1, flag, flag.size() - 1)) elementsToRemove.push_back(i);
     }
-    int offset=0;
-    for(int index : elementsToRemove) 
+    int offset = 0;
+    for(int index : elementsToRemove)
     {
-        frame.erase(frame.begin()+offset+index);
+        frame.erase(frame.begin() + offset+index);
         offset--;
     }
 }
@@ -62,46 +62,45 @@ void DataLinkLayer::revBitStuff(vector<bool> &frame)
 
 void DataLinkLayer::setPadding(vector<bool> &frame)
 {
-    int lengthOfPadding=0;
-    
-    while(frame.size()%4)
+    int lengthOfPadding = 0;
+
+    while(frame.size() % 4)
     {
         lengthOfPadding++;
         frame.push_back(0);
     }
-    frame.insert(frame.begin(),lengthOfPadding%2);
-    frame.insert(frame.begin(),lengthOfPadding%4-lengthOfPadding%2);
+    frame.insert(frame.begin(), lengthOfPadding % 2);
+    frame.insert(frame.begin(), lengthOfPadding % 4 - lengthOfPadding % 2);
 }
 
 void DataLinkLayer::removePadding(vector<bool> &frame)
 {
-    int paddinglength=(frame[0]<<1)+(frame[1]);
-    frame.erase(frame.begin(),frame.begin()+2);
-    for(int i=0;i<paddinglength;i++) frame.pop_back();
+    int paddinglength = (frame[0]<<1) + frame[1];
+    frame.erase(frame.begin(), frame.begin() + 2);
+    for(int i = 0; i < paddinglength; i++) frame.pop_back();
 }
 
 bool DataLinkLayer::getID(vector<bool> &frame)
 {
-    int ID=frame[0];
+    int ID = frame[0];
     frame.erase(frame.begin());
-    if (lastinID==ID) return false;
-    lastinID=ID;
+    if (lastinID == ID) return false;
+    lastinID = ID;
     return true;
 }
 
-void DataLinkLayer::setID(vector<bool> &frame,bool ID)
+void DataLinkLayer::setID(vector<bool> &frame)
 {
-    frame.insert(frame.begin(),ID);
-    lastoutID=ID; 
+    lastoutID = lastoutID ^ 1;
+    frame.insert(frame.begin(), lastoutID);
 }
 
 
-bool flagcheck(vector<bool> &vec1, int start1,array<bool,8> &flag,int lenght) //check if the flag matches given vec
+bool flagcheck(vector<bool> &vec1, int start1,array<bool, 8> &flag, int lenght) //check if the flag matches given vec
 {
-    for(int i=0;i<lenght;i++)
+    for(int i = 0 ; i < lenght; i++)
     {
-        if (vec1[start1+i]!=flag[i]) return false;
-        
+        if (vec1[start1 + i] != flag[i]) return false;
     }
     return true;
 }
--- DataLinkLayer.h
+++ DataLinkLayer.h
@@ -9,15 +9,15 @@
 Layout of frame:
 |--|----|-----------------|----|---|
 |ID|Type|Length of padding|Data|CRC|
-|1 | 3  |       2         |  ? |8? |   
+|1 | 3  |       2         |  ? |8? |
 |--|----|-----------------|----|---|
 */
 using namespace std;
-struct Buffer 
+struct Buffer
 {
-    array<vector<bool>,BUFFERSIZE> Buffer;
-    int head=0;
-    int tail=0; 
+    array<vector<bool>, BUFFERSIZE> Buffer;
+    int head = 0;
+    int tail = 0;
 };
 
 class DataLinkLayer
@@ -25,23 +25,23 @@ class DataLinkLayer
     public:
         void bitStuff(vector<bool> &frame); //Stuff frame to avoid flags in data.
         void revBitStuff(vector<bool> &frame); //Remove stuffing.
-        
+
         void setPadding(vector<bool> &frame); //Pad to multiple of 4, and set length of padding field.
         void removePadding(vector<bool> &frame); //Remove padding.. and padding field.
-        
+
         void CRCencoder(vector<bool> &dataWord); //Make dataword into codeword. (append CRC)
         bool CRCdecoder(vector<bool> &codeWord); //Make codeword into dataword, discard frame if corrupt. return false on fail, else true.
-        
+
         bool getID(vector<bool> &frame); //Get id of frame, discard if same as lastID
-        void setID(vector<bool> &frame,bool ID); //set id of frame.
-        
-        
+        void setID(vector<bool> &frame); //set id of frame.
+
+
         int getType(vector<bool> &frame); //Get type of frame.
         void setType(vector<bool> &frame,int type); //Set type of frame.
-        
+
         void setTimer(); //Set the timer
         void sendACK(bool ID); //send ACK.
-    
+
     public:
         DataLinkLayer();
         bool dataAvaliable(); //Is there new data for AppLayer?
@@ -50,12 +50,12 @@ class DataLinkLayer
         void pushData(vector<bool>); //push data from AppLayer to outBuffer
         void getFrames(); //Grab frames from physical layer, parse to AppLayer if reqiured.
         void getDatagrams(); //Grab frames from inBuffer and parse to physical layer.
-    
+
     private:
         //physicalLayer physLayer;
         bool lastinID;
         bool lastoutID;
-        array<bool,8> flag={{1,0,1,0 ,1,1,1,0}};
+        array<bool, 8> flag={{1, 0, 1, 0, 1, 1, 1, 0}};
         timeval timer;
         Buffer inBuffer;
         Buffer outBuffer;
@@ -66,6 +66,6 @@ class DataLinkLayer
 void getFramesWrapper(DataLinkLayer *DaLLObj);
 void getDatagramsWraper(DataLinkLayer *DaLLObj);
 
-bool flagcheck(vector<bool> &vec1, int start1,array<bool,8> &flag,int lenght);
+bool flagcheck(vector<bool> &vec1, int start1,array<bool, 8> &flag, int lenght);
 
 
