5ae8bb7ed2ef7c2ced8f4f43c400d0232abea82d
FrederikHaa <frhaa13@student.sdu.dk>
2014-10-08 21:23:52 +0200

    small changes to NewPhysicalLayer.h

--- NewPhysicalLayer.h
+++ NewPhysicalLayer.h
@@ -1,12 +1,15 @@
 #include    <vector>
 using namespace std;
 
+
+
+
 class   NewPhysicalLayer
 {
 private:
-	void			applyHamming();
-	void			applyEndFlags();
-	void			removeEndFlags();
+	void			applyHamming(vector<float>);
+	void			applyStartEndFlags();
+	void			removeStartEndFlags();
 	vector<string>	convertToDTMF(vector<bool>);
 	vector<bool>	convertToBinary(vector<string>);
 	void			playSync();
@@ -31,8 +34,8 @@ private:
         int Freqarray1[4]={697,770,852,941};
         int Freqarray2[4]={1209,1336,1477,1633};
         char SyncSequence[4]={'a','6','8','*'}; //Should not contain the same note two times in a row.
-        char SyncEnd[4]={'1','2','3','4'};
-        char EndSequence[4]={'*','8','6','a'};
+        char startFlag[4]={'1','2','3','4'};
+        char endFlag[4]={'*','8','6','a'};
         char DTMFTones[16]={'1','2','3','a','4','5','6','b','7','8','9','c','*','0','#','d'};
         DualTonePlayer Player;
         Recorder Rec;
