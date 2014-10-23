73be360c269282b75802022b42c9900ed241d9d0
FrederikHaa <frhaa13@student.sdu.dk>
2014-10-23 11:27:28 +0200

    edited main so program compiles

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
-#include "physicalLayer.h"
+#include "NewPhysicalLayer.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    physicalLayer PhysLayer;
-    PhysLayer.QueueFrame("25264abc863");
 
-    while(1)
-    {
-        if(PhysLayer.isFrameAvaliable())
-        {
-            cout << PhysLayer.GetNextFrame() << endl;
-        }
-        usleep(100000);
-    }
 }
