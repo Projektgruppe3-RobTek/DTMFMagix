11c831c3d8e1ca0fd412b474670194fdbe909096
FrederikHaa <frhaa13@student.sdu.dk>
2014-10-23 13:13:23 +0200

    fixes

--- NewPhysicalLayer.h
+++ NewPhysicalLayer.h
@@ -2,7 +2,6 @@
 #include	"Player.h"
 #include 	"Goertzel.h"
 #include    <vector>
-#include	<string>
 #include 	<thread>
 using namespace std;
 
@@ -13,12 +12,10 @@ class   NewPhysicalLayer
 {
 private:
 	void			applyHamming(vector<float>);
-	void			applyStartEndFlags();
-	void			removeStartEndFlags();
-	vector<string>	convertToDTMF(vector<bool>);
-	vector<bool>	convertToBinary(vector<string>);
-	void			playSync();
-	void			getSync();
+	void			applyPreambleTrailer();
+	void			removePreambleTrailer();
+	vector<char>	convertToDTMF(vector<bool>);
+	vector<bool>	convertToBinary(vector<char>);
 	void			playFrame(vector<bool>);
 	void			getFrame();
 
@@ -26,30 +23,27 @@ public:
 
     NewPhysicalLayer();
     bool			returnReceiveFlag();
+    bool            returnSendFlag();
     bool            isFrameAvailable();
     vector<bool>    popFrame();
-
-    bool			returnSendFlag();
     void            pushFrame(vector<bool>);
-
-    ~NewPhysicalLayer();
-
-
 
 private:
 
-        int Freqarray1[4]={697,770,852,941};
-        int Freqarray2[4]={1209,1336,1477,1633};
-        char SyncSequence[4]={'a','6','8','*'}; //Should not contain the same note two times in a row.
-        char startFlag[4]={'1','2','3','4'};
-        char endFlag[4]={'*','8','6','a'};
-        char DTMFTones[16]={'1','2','3','a','4','5','6','b','7','8','9','c','*','0','#','d'};
-        DualTonePlayer Player;
-        Recorder Rec;
-        int samplesSinceSync=0;
-        long long synctime;
-        thread incommingThread;
-        thread outgoingThread;
-        vector<bool> incommingFrame
-        vector<bool> outgoingFrame
+    int             Freqarray1[4]={697,770,852,941};
+    int             Freqarray2[4]={1209,1336,1477,1633};
+    char            SyncSequence[4]={'a','6','8','*'}; //Should not contain the same note two times in a row.
+    char            startFlag[4]={'1','2','3','4'};
+    char            endFlag[4]={'*','8','6','a'};
+    char            DTMFTones[16]={'0','1','2','3','4','5','6','7','8','9','#','d'};
+    DualTonePlayer  Player;
+    Recorder        Rec;
+    int             samplesSinceSync=0;
+    long long       synctime;
+    bool            receiveFlag;
+    bool            sendFlag;
+    thread          incommingThread;
+    thread          outgoingThread;
+    vector<char>    incommingFrame;
+    vector<char>    outgoingFrame;
 };
