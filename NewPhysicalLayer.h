7ff8b0902613ea5309c94b6b9be11d426f7b03ff
nwl1515 <nw.leth@gmail.com>
2014-10-30 14:50:52 +0100

    revert fails

--- NewPhysicalLayer.h
+++ NewPhysicalLayer.h
@@ -2,23 +2,21 @@
 #include	"Player.h"
 #include 	"Goertzel.h"
 #include    <vector>
-#include	<string>
 #include 	<thread>
+#define TONELENGHT 100
+#define SILENTLENGHT 20
+#ifndef M_PI
+#define M_PI        3.14159265358979323846
+#endif
 using namespace std;
-
-
-
 
 class   NewPhysicalLayer
 {
 private:
 	void			applyHamming(vector<float>);
-	void			applyStartEndFlags();
-	void			removeStartEndFlags();
-	vector<string>	convertToDTMF(vector<bool>);
-	vector<bool>	convertToBinary(vector<string>);
-	void			playSync();
-	void			getSync();
+	vector<char>	applyPreambleTrailer(vector<char>);
+	vector<char>	convertToDTMF(vector<bool>);
+	vector<bool>	convertToBinary(vector<char>);
 	void			playFrame(vector<bool>);
 	void			getFrame();
 
@@ -26,30 +24,27 @@ public:
 
     NewPhysicalLayer();
     bool			returnReceiveFlag();
+    bool            returnSendFlag();
     bool            isFrameAvailable();
     vector<bool>    popFrame();
-
-    bool			returnSendFlag();
     void            pushFrame(vector<bool>);
-
-    ~NewPhysicalLayer();
-
-
 
 private:
 
-        int Freqarray1[4]={697,770,852,941};
-        int Freqarray2[4]={1209,1336,1477,1633};
-        char SyncSequence[4]={'a','6','8','*'}; //Should not contain the same note two times in a row.
-        char startFlag[4]={'1','2','3','4'};
-        char endFlag[4]={'*','8','6','a'};
-        char DTMFTones[16]={'1','2','3','a','4','5','6','b','7','8','9','c','*','0','#','d'};
-        DualTonePlayer Player;
-        Recorder Rec;
-        int samplesSinceSync=0;
-        long long synctime;
-        thread incommingThread;
-        thread outgoingThread;
-        vector<bool> incommingFrame
-        vector<bool> outgoingFrame
+    int             Freqarray1[4]={697,770,852,941};
+    int             Freqarray2[4]={1209,1336,1477,1633};
+    char            SyncSequence[4]={'a','6','8','*'}; //Should not contain the same note two times in a row.
+    char            startFlag[4]={'1','2','3','4'};
+    char            endFlag[4]={'*','8','6','a'};
+    char            DTMFTones[16]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','*','#'};
+    DualTonePlayer  Player;
+    Recorder        Rec;
+    int             samplesSinceSync=0;
+    long long       synctime;
+    bool            receiveFlag;
+    bool            sendFlag;
+    thread          incommingThread;
+    thread          outgoingThread;
+    vector<char>    incommingFrame;
+    vector<char>    outgoingFrame;
 };
