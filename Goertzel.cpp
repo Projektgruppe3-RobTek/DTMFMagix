a958c41a312c60bf9f4eff60e9972b2f2aa06efd
StefanRvO <stefan@stefanrvo.dk>
2014-10-30 14:00:15 +0100

    Rewrote Goertzel transform

--- Goertzel.cpp
+++ Goertzel.cpp
@@ -1,37 +1,33 @@
-#include "Goertzel.h"
-using namespace std;
-float goertzel_mag(int TARGET_FREQUENCY,int SAMPLING_RATE, vector<float> &data)  //This implementation is taken/borowed/stolen/etc. from Stackoverflow.
-                                                                                        //It would be wise to rewrite it. Shouldn't be too difficult.
+#include <cmath>
+#include <vector>
+#include <iostream>
+#ifndef M_PI
+#define M_PI 3.1415926535f
+#endif
+/*Inspired by instructions at
+**https://courses.cs.washington.edu/courses/cse466/12au/calendar/Goertzel-EETimes.pdf
+*/
+
+float doGoertzel(int sampleRate, float targetFreq, std::vector<float> &samples)
 {
-    int numSamples=data.size();
-    int     k,i;
-    float   floatnumSamples;
-    float   omega,sine,cosine,coeff,q0,q1,q2,magnitude,real,imag;
-
-    float   scalingFactor = numSamples / 2.0;
-
-    floatnumSamples = (float) numSamples;
-    k = (int) (0.5 + ((floatnumSamples * TARGET_FREQUENCY) / SAMPLING_RATE));
-    omega = (2.0 * M_PI * k) / floatnumSamples;
-    sine = sin(omega);
-    cosine = cos(omega);
-    coeff = 2.0 * cosine;
-    q0=0;
-    q1=0;
-    q2=0;
-
-    for(i=0; i<numSamples; i++)
-    {
-        q0 = coeff * q1 - q2 + data[i];
+    //Define constants
+    float k = (int) (0.5 + ((float(samples.size()) * targetFreq) / sampleRate));
+    float w = ((2. * M_PI)  / (int)samples.size())*k ;
+    float cosine=cos(w);
+    float sine=sin(w);
+    float coeff=2.*cosine;
+    
+    //declare sample variables
+    float q0 = 0.; //this sample
+    float q1 = 0.; //last sample
+    float q2 = 0.; //sample two times ago
+    
+    //Compute goertzel transform
+    for (int i = 0; i < samples.size(); i++){ 
+        q0 = coeff * q1 - q2 + samples[i];
         q2 = q1;
         q1 = q0;
-    }
-
-    // calculate the real and imaginary results
-    // scaling appropriately
-    real = (q1 - q2 * cosine) / scalingFactor;
-    imag = (q2 * sine) / scalingFactor;
-
-    magnitude = sqrt(real*real + imag*imag);
-    return magnitude;
-    }
+    } 
+    
+    return sqrt(q1*q1 + q2*q2 - q1 * q2 * coeff); //return power 
+}
--- Goertzel.h
+++ Goertzel.h
@@ -1,6 +1,1 @@
-#include <cmath>
-#include <vector>
-#ifndef M_PI
-#define M_PI        3.14159265358979323846
-#endif
-float goertzel_mag(int TARGET_FREQUENCY,int SAMPLING_RATE, std::vector<float> &data);
+float doGoertzel(int sampleRate, float targetFreq, std::vector<float> &samples);
