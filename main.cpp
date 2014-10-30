d8cb3d11c2ab4a339d21fa5048e9bc8829b5485f
nwl1515 <nw.leth@gmail.com>
2014-10-30 14:24:18 +0100

    Main for test of player

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include "physicalLayer.h"
+//#include "physicalLayer.h"
+#include "NewPlayer.h"
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    physicalLayer PhysLayer;
-    PhysLayer.QueueFrame("25264abc863");
+  /*   physicalLayer PhysLayer;
+    PhysLayer.QueueFrame("2");
 
     while(1)
     {
@@ -13,6 +15,31 @@ int main(int argc, char *argv[])
         {
             cout << PhysLayer.GetNextFrame() << endl;
         }
-        usleep(100000);
+        usleep(5000);
+
     }
+
+*/
+
+    DTMFPlayer test;
+
+//while(true)
+
+
+while(true)
+{
+
+
+    test.PlayDTMF({'1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'},500,20);
+}
+
+
+
+
+    return 0;
+
+
+
+
+
 }
