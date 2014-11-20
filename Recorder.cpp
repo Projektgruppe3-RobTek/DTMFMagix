afa7d7a570831f57ada95b19e6ceb8dd017ef9bd
nwl1515 <nw.leth@gmail.com>
2014-11-20 15:26:15 +0100

    Old recorder

--- Recorder.cpp
+++ Recorder.cpp
@@ -1,91 +1,92 @@
 #include "Recorder.h"
 
-
-DTMFRecorder::DTMFRecorder()
+using namespace std;
+/* This routine will be called by the PortAudio engine when audio is needed.
+It may called at interrupt level on some machines so don't do anything
+that could mess up the system like calling malloc() or free().
+*/
+int Recorder::patestCallback(   const void *inputBuffer,void *outputBuffer,
+                                unsigned long framesPerBuffer,
+                                const PaStreamCallbackTimeInfo *timeInfo,
+                                PaStreamCallbackFlags statusFlags,
+                                void *userData )
 {
-    RecData=new RingBuffer<float,INPUTBUFFERSIZE>;
-    cout << "test";
-    PaStream *stream;
-    err = Pa_Initialize();
-    if (err != paNoError)
+    PortAudioData *data=(PortAudioData*) userData;
+    //float *out =(float*) outputBuffer;
+    float *in =(float*)inputBuffer;
+    if (in==NULL) cout <<"ERROR Input buffer was 0!!" << endl;
+    
+    for(unsigned int i=0;i<framesPerBuffer;i++)
     {
-        cout << Pa_GetErrorText(err) << endl;
-        err = Pa_Terminate();
-    }
-
-     err = Pa_OpenDefaultStream(  &stream,
-                            INPUTCHANNELS, /* input channels */
-                           OUTPUTCHANNELS, /* output channels */
-                                paFloat32, /* 32 bit floating point output */
-                          REC_SAMPLE_RATE, /* Sample Rate */
-                          REC_BUFFER_SIZE, /* frames per buffer */
-                           RecCallback, /* this is your callback function */
-                                  RecData ); /* Point passed to callback */
-
-    if (err != paNoError)
-    {
-        cout << Pa_GetErrorText(err) << endl;
-        err = Pa_Terminate();
-    }
-
-    err = Pa_StartStream(stream);
-    if (err != paNoError)
-    {
-
-        cout << Pa_GetErrorText(err) << endl;
-        err = Pa_Terminate();
-    }
-
-
-}
-
-int DTMFRecorder::RecCallback( const void *inputBuffer,void *outputBuffer,unsigned long framePerBuffer,
-                    const PaStreamCallbackTimeInfo* timeInfo,PaStreamCallbackFlags statusFlags,
-                    void *userData )
-{
-    RingBuffer<float,INPUTBUFFERSIZE> *data = (RingBuffer<float,INPUTBUFFERSIZE>*)userData;
-    float *in = (float*)inputBuffer;
-    (void) outputBuffer;        /* Prevent unused variable warning. */
-    for(unsigned int i = 0; i< framePerBuffer; i++)
-    {
-    data->push_back(in[i]);
+        data->recBuff[data->NextRec++]=in[i]; //Maybe don't count NextRec up in the loop?? May cause problems
+        if (data->NextRec>=SAMPLE_RATE*RECBUFLENGHT) data-> NextRec=0;
     }
     return paContinue;
-
-
 }
 
-vector<float> DTMFRecorder::GetAudioData(int lenght ,int OffSet)
-{
-    vector<float> Samples;   // Output vector
-    RingBuffer<float,INPUTBUFFERSIZE> *copyBuf = RecData; //make a copy of Input buffer
-    if ((REC_SAMPLE_RATE*(OffSet+lenght)/1000) < INPUTBUFFERSIZE)
-        {
-            copyBuf->turn(REC_SAMPLE_RATE*(OffSet+lenght)/1000);             //Turn ringbuffer for offset
-        }
-        else
-        {
-            cout << "OFFSet to big" << endl;
-        }
-    for(int i = 0; i< (REC_SAMPLE_RATE*lenght/1000);i++)
-    {
-        Samples.push_back(copyBuf->pop_front());                     // Copy desired samples to Samples
-    }
 
+Recorder::Recorder()
+{
+    PaError err;
+    //Init PortAudio
+    err = Pa_Initialize();
+    if(err != paNoError) {cout << Pa_GetErrorText( err ) << endl; err = Pa_Terminate(); }
+    //Create stream 1 input channel
+    err=Pa_OpenDefaultStream (&stream, 
+                                1, //input channels
+                                0,
+                                paFloat32,
+                                SAMPLE_RATE,
+                                10, //This may need to be changed (lowered, but this may introduce noise/artifacts. Testing is needed)
+                                    //Setting to one apperently don't cause artefacts (need confirmation), but much greater CPU usage.
+                                patestCallback,
+                                &RecData );
+    if(err != paNoError) {cout << Pa_GetErrorText( err ) << endl; err = Pa_Terminate(); }
+    err = Pa_StartStream(stream);
+    for(unsigned int i=0;i<RecData.recBuff.size();i++) RecData.recBuff[i]=0;
+    cout << "Initialised audio recorder!" << endl;
+    cout << "We use PortAudio version " << Pa_GetVersionText() << endl;
+}
+
+
+vector<float> Recorder::GetAudioData(int lenght ,int OffSet) /*OffSet= Get data from x milliseconds ago. Lenght=Lenght of recorded audio in ms*/ 
+{
+    //The buffer only have a certain lenght, don't request samples which has been thrown away!
+    //Also, don't request future samples!
+    if (OffSet+lenght>(RECBUFLENGHT*1000)*0.9 or OffSet<0) 
+    {   
+        cout << "Error getting Audio Data!!! Don't request Samples we don't have!!!" << endl;
+        vector<float> rvec;
+        return rvec;
+    }
+    //endpoint is the last sample to put in our array
+    int endPoint=RecData.NextRec;
+    endPoint -=(SAMPLE_RATE*OffSet)/1000;
+    if (endPoint<0) endPoint+=SAMPLE_RATE*RECBUFLENGHT;
+    //startPoint is the first sample to put in our array
+    int startPoint=endPoint-(SAMPLE_RATE*lenght)/1000;
+    if (startPoint<0) startPoint+=SAMPLE_RATE*RECBUFLENGHT;
+    //Now, put the requsted samples into a array
+    int i=startPoint;
+    vector<float> Samples;
+    Samples.reserve((SAMPLE_RATE*lenght)/1000);
+    while(i!=endPoint)
+    {
+        //cout <<i << endl;
+        Samples.push_back(RecData.recBuff[i]);
+        i++;
+        if (i==SAMPLE_RATE*RECBUFLENGHT) i=0;
+    }
     return Samples;
 
-
-
-
 }
 
-DTMFRecorder::~DTMFRecorder()
+
+Recorder::~Recorder()
 {
-    delete RecData;
-    err = Pa_Terminate();
-    if (err !=  paNoError)
-    {
-        cout << Pa_GetErrorText(err) << endl;
-    }
+    PaError err;
+    err = Pa_CloseStream( stream );
+    if(err != paNoError) {cout << Pa_GetErrorText( err ) << endl; err = Pa_Terminate(); }
+    err = Pa_Terminate( );
+    if(err != paNoError) {cout << Pa_GetErrorText( err ) << endl; err = Pa_Terminate(); }
 }
-
--- Recorder.h
+++ Recorder.h
@@ -1,40 +1,32 @@
-#include <iostream>
-#include <portaudio.h>
-#include "RingBuffer.h"
+#include<portaudio.h>
+#include<iostream>
+#include<cmath>
+#include<array>
+#include <ctime>
 #include <vector>
-#define INPUTBUFFERSIZE 882000   // 2* sample rate
-#define REC_SAMPLE_RATE 8000
-#define INPUTCHANNELS 1
-#define OUTPUTCHANNELS 0
-#define REC_BUFFER_SIZE 8
-
-#pragma once
-using namespace std;
-
-
-class DTMFRecorder{
-
-public:
-    DTMFRecorder();
-    vector<float> GetAudioData(int lenght ,int OffSet);
-
-    ~DTMFRecorder();
-
-
+#define SAMPLE_RATE 44100
+#define RECBUFLENGHT 10
+#ifndef M_PI
+#define M_PI        3.14159265358979323846
+#endif
+typedef struct
+{
+    std::array<float,SAMPLE_RATE*RECBUFLENGHT> recBuff; //don't do this! may cause lag! don't use dynamic memory. Maybe use a ringbuffer instead. Size?    
+    int NextRec=0;
+} PortAudioData;
+class Recorder
+{
 private:
-    PaError err;
-    RingBuffer<float,INPUTBUFFERSIZE> *RecData;
-    static int RecCallback( const void *inputBuffer,void *outputBuffer,unsigned long framePerBuffer,
-                    const PaStreamCallbackTimeInfo* timeInfo,PaStreamCallbackFlags statusFlags,
-                    void *userData ) ;
-
-
-
-
-
-
-
-
-
+    static int patestCallback(  const void *inputBuffer,void *outputBuffer,
+                            unsigned long framesPerBuffer,
+                            const PaStreamCallbackTimeInfo *timeInfo,
+                            PaStreamCallbackFlags statusFlags,
+                            void *userData );
+    PaStream *stream;
+    PortAudioData RecData;
+public:
+    Recorder();
+    ~Recorder();
+    std::vector<float> GetAudioData(int lenght ,int OffSet);
 
 };
